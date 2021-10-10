b059218a5323547349eb97faa99c4655249c72ca
Piotr <p.jaronsky@gmail.com>
2021-10-10 11:53:48 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,16 +1,16 @@
-# First, update apt
+# First, update apt ------------------------------
 sudo apt update
 
-# Apt installs: ZSH, Git, Curl
-sudo apt install zsh git curl
+# Apt installs -----------------------------------
+sudo apt install zsh git curl wget
 
-# Oh My ZSH
+# Oh My ZSH --------------------------------------
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-# Snap installs: VLC, Discord
+# Snap installs ----------------------------------
 sudo snap install vlc discord
 
-# Manual installs
+# Manual installs --------------------------------
 
 # Brave Web Browser, copied from https://brave.com/linux/#linux
 sudo apt install apt-transport-https curl
@@ -19,12 +19,20 @@ echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=
 sudo apt update
 sudo apt install brave-browser
 
-# Streamio (might have to select an updated version)
-curl https://dl.strem.io/shell-linux/v4.4.137/stremio_4.4.137-1_amd64.deb --output stremio.deb
+# Streamio
+# TODO replace hardcoded version with environment variable
+wget https://dl.strem.io/shell-linux/v4.4.137/stremio_4.4.137-1_amd64.deb
 # will most likely fail, this is ok
 set +e
-sudo dpkg -i stremio.deb
+sudo dpkg -i stremio_4.4.137-1_amd64.deb
 set -e
 # here we fix the dependencies
 sudo apt -f install
-rm -f stremio.deb
+rm -f stremio_4.4.137-1_amd64.deb
+
+# Jetbrains Toolbox
+# TODO replace hardcoded version with environment variable
+wget https://download.jetbrains.com/toolbox/jetbrains-toolbox-1.21.9712.tar.gz
+tar -xvzf jetbrains-toolbox-1.21.9712.tar.gz
+rm -f jetbrains-toolbox-1.21.9712.tar.gz
+./jetbrains-toolbox-1.21.9712/jetbrains-toolbox
